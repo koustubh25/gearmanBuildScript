9c011fa05768c6cfb1305f85c02f9f20a564b593
Koustubh <koustubh@eviry.com>
2015-11-05 10:54:32 +0900

    Fixed php installation dir issue

--- install_gearman_manager.sh
+++ install_gearman_manager.sh
@@ -9,14 +9,15 @@ source GLOBAL
 # check if run as root
 check_root
 
-cd ${GEARMAN_MANAGER_DOWNLOAD_DIR}
+
 install_program "git" "git-core"
 
 #check php version
 if hash php 2>/dev/null; then
 	echo "PHP found."
 else
-	read -e -p "This script needs PHP. Press y to install" -i "y" ans 
+	read -e -p "
+	This script needs PHP. Press y to install" -i "y" ans 
 	if [ "$ans" == "y" ]; then
 		./install_php.sh
 	else
@@ -28,6 +29,8 @@ fi
 echo "checking PHP version..."
 PHP_VERSION_FOUND=$(php -v|grep --only-matching --perl-regexp "5\.\\d+\.\\d+")
 version=$(compare_versions $GEARMAN_MANAGER_MASTER_BRANCH_VERSION $PHP_VERSION_FOUND)
+
+cd ${GEARMAN_MANAGER_DOWNLOAD_DIR}
 
 rm -rf ${GearmanManager}GearmanManager
 
@@ -42,7 +45,8 @@ else
 	#clone v2
 	git clone -b v2 ${GEARMAN_MANAGER_SOURCE}
 
-	read -e -p "You need pcntl enabled in php to use gearman manager. Press y to continue." -i "y" ans
+	read -e -p "
+	You need pcntl enabled in php to use gearman manager. Press y to continue." -i "y" ans
 	if [ "$ans" != "y" ];then
 		exit 1;
 	fi
@@ -71,6 +75,7 @@ else
 	make install
 	verify_command $? "Error installing php pcntl"
 
-	echo "Make sure to add absolute path of pcntl.so in php.ini file"
+	echo "
+	Make sure to add absolute path of pcntl.so in php.ini file"
 
 fi
